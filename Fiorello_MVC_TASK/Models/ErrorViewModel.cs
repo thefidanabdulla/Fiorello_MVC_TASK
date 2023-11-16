c3e91dfb1f5f2c99a007f9cefc268b3e6e07e857
thefidanabdulla <thefidanabdulla@gmail.com>
2023-11-16 11:25:01 +0400

    first commit

---
+++ Fiorello_MVC_TASK/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Fiorello_MVC_TASK.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
